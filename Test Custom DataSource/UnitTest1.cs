fdfae0c8004152a9b7bcf1318eedfd6b7c322af7
iJugleboy <daniel.mettler@2sic.com>
2015-05-09 13:24:46 +0200

    Created demo data source and test code

---
+++ Test Custom DataSource/UnitTest1.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyCompany.DataSources;
+using ToSic.Eav.ValueProvider;
+
+namespace YourCompany.Testing.eav.DataSources
+{
+    [TestClass]
+    public class DateTimeDataSourceTest
+    {
+        [TestMethod]
+        public void TestStandardUseCase()
+        {
+            var yourDataSource = TestSource1();
+            Assert.AreEqual(1, yourDataSource.Out.Count, "Should only find one out-stream");
+            Assert.AreEqual(1, yourDataSource.List.Count);
+
+            var first = yourDataSource.List.FirstOrDefault().Value; 
+            Assert.AreEqual("Date Today", first.GetBestValue("Title"));
+            Assert.AreEqual("Date Today", first.GetBestValue("EntityTitle"));
+            Assert.AreEqual("Saturday", first.GetBestValue("DayOfWeek"), "Expecting it to be Saturday - you'll have to update this test for your weekday.");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(System.Exception))]
+        public void UseInvalidSetting_ExpectError()
+        {
+            var ds = TestSource1();
+            var settings = new Dictionary<string, string>();
+            settings.Add("DesiredDate", "Not today - should cause error");
+            var settingsValueProvider = new StaticValueProvider("Settings", settings);
+            ds.ConfigurationProvider.Sources.Add(settingsValueProvider.Name, settingsValueProvider);
+            Assert.AreEqual("Date Today", ds.List);
+        }
+
+        public DateTimeDataSource TestSource1()
+        {
+            var testDS = ToSic.Eav.DataSource.GetDataSource<DateTimeDataSource>(1, 1, null, TestConfigProvider());
+            return testDS;
+        }
+
+
+
+        /// <summary>
+        /// Create a test config provider - here you could supply tokens if you want to run tests which would resolve a token for you
+        /// </summary>
+        /// <returns></returns>
+        public IValueCollectionProvider TestConfigProvider()
+        {
+            var vc = new ValueCollectionProvider();
+            // var entVc = new EntityValueProvider(AppSettings(), "AppSettings");
+            // vc.Sources.Add("AppSettings".ToLower(), entVc);
+            // vc.Sources.Add("AppResources".ToLower(), new EntityValueProvider(AppResources(), "AppResources"));
+            return vc;
+        }
+
+
+    }
+}
---
+++ Your Custom DataSource/DateTimeDataSource.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ToSic.Eav;
+using ToSic.Eav.DataSources;
+
+namespace MyCompany.DataSources
+{
+    public class DateTimeDataSource: BaseDataSource
+    {
+        #region Configuration-properties
+		private const string SomeDemoConfigurationKey = "DemoConfigNameInConfigList";
+        private const string AnotherDemoConfigKey = "AnotherDemoConfig";
+
+		/// <summary>
+		/// A piece of demo-configuration. It must always be stored/accessed from the Configuration dictionary
+		/// because everythin in the config-dictionary will be token-resolved
+		/// </summary>
+		public string DemoConfiguration
+		{
+			get { return Configuration[SomeDemoConfigurationKey]; }
+			set { Configuration[SomeDemoConfigurationKey] = value; }
+		}
+
+        /// <summary>
+        /// A number-demo config. Note that we don't do error-checking, because if it's actuall not a number, a error should really be raised. 
+        /// You'll see later that we're actually starting with a token, but we're already adding a fallback number so after token resolving
+        /// it should always be a number
+        /// </summary>
+        public int AnotherDemoConfig
+        {
+            get { return Convert.ToInt32(Configuration[AnotherDemoConfigKey]); }
+            set { Configuration[AnotherDemoConfigKey] = value.ToString(); }
+        }
+
+		#endregion
+
+		/// <summary>
+		/// Constructs a new EntityIdFilter
+		/// </summary>
+		public DateTimeDataSource()
+		{
+            // The out-list contains all out-streams.
+            // For performance reasons we want to make sure that they are NOT created unless accessed
+            // Because of this, we create a data-stream with a deferred call to GetEntities - like this:
+			Out.Add(DataSource.DefaultStreamName, new DataStream(this, DataSource.DefaultStreamName, GetEntities));
+
+            // Example of pre-configuring a text
+            // This will place the token to be resolved into the variable
+            // The tokens will be resolved before use
+            // The following token means: 
+            // - Try to use the configured value from the setting on this data-source in the visual query
+            // - if there is none, just use the value "Today"
+		    DemoConfiguration = "[Settings:DesiredDate||Today]"; 
+            
+            // Example of pre-configuring a number value
+            // We can't just say AnotherDemoConfig = "text" because that would not compile since it expects a number
+            // So we just add the token to be resolved directly to the configuration list
+            Configuration.Add(AnotherDemoConfigKey, "[Settings:Hours||17]"); 
+		}
+
+
+        private Dictionary<int, IEntity> _cachedEntities; 
+
+        /// <summary>
+        /// This is the deferred call to retrieve entities
+        /// If you created the source correctly it won't be called unless accessed
+        /// This is recommended for performance reasons
+        /// We also recommend placing the result in the cache...
+        /// </summary>
+        /// <returns></returns>
+		private IDictionary<int, IEntity> GetEntities()
+        {
+            // try to use the cached result in case this had been accessed before
+            if (_cachedEntities != null)
+                return _cachedEntities;
+
+            // This will resolve the tokens before starting
+			EnsureConfigurationIsLoaded();
+
+            // Here's your real code. 
+            // Typically you will either perform some work with the In-streams
+            // or retrieve data from another source like XML, RSS, SQL, File-storage etc.
+            // Usually you would also need configuration from the UI - but sometimes not, especially if it's just for a very specific purpose
+			#region Your Custom Business Logic
+
+            _cachedEntities = new Dictionary<int, IEntity>();
+			try
+			{
+                // Check if we're trying to inform about today
+			    if (DemoConfiguration != "Today")
+                    throw new Exception();
+
+                // In this demo we'll just create 1 entity containing some values related to today
+                // I'll use the simple method of placing all values in a dictionary and then converting it into an Entity-object
+			    var today = new Dictionary<string, object>();
+                today.Add("Title", "Date Today");
+                today.Add("Date", DateTime.Today);
+                today.Add("DayOfWeek", DateTime.Today.DayOfWeek.ToString());
+                today.Add("DayOfWeekNumber", DateTime.Today.DayOfWeek);
+
+                // ...now convert to an entity, and add to the list of results
+			    var ent = new ToSic.Eav.Data.Entity(1, "Date", today, "Title");
+                _cachedEntities.Add(ent.EntityId, ent);
+			}
+			catch (Exception ex)
+			{
+				throw new Exception("Unable to generate entities in this custom data source", ex);
+			}
+			#endregion
+
+			return _cachedEntities;
+		}
+    }
+}
